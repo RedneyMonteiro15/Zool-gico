b0eaee2bcca8a4698fb71da6dc7d434df63fcd46
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2021-02-25 19:57:50 +0000

    classe animais criado
    foi criado algumas classes, animais, mamiferos, aves, peixes e repteis com os seus atributos

---
+++ zoologico/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace zoologico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Mamiferos m = new Mamiferos("Cão", "Latir", 8, "Ração");
+            m.listarMamifero();
+        }
+    }
+}
+class Animais
+{
+    string nome, descricao;
+    int quantidade;
+
+}
+class Mamiferos
+{
+    string nome, descricao, alimento;
+    int quantidade;
+    public Mamiferos(string nome, string descricao, int quantidade, string alimento)
+    {
+        nome = this.nome;
+        descricao = this.descricao;
+        quantidade = this.quantidade;
+        alimento = this.alimento;
+    }
+    public void listarMamifero()
+    {
+        Console.WriteLine("--------------------");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Lista dos Mamiferos");
+        Console.ResetColor();
+        Console.WriteLine("--------------------");
+        Console.WriteLine("Nome: " + this.nome);
+        Console.WriteLine("Descrição: {0}" + descricao);
+        Console.WriteLine("Quantidade: {0}", quantidade);
+        Console.WriteLine("Alimento: {0}", alimento);
+        Console.WriteLine("--------------------");
+    }
+
+}
+class Aves
+{
+
+}
+class Peixes
+{
+
+}
+class Repteis
+{
+
+}
