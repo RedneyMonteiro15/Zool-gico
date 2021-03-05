49be4699266c48933c599f73d7681d0e5770bde5
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2021-03-05 21:24:18 +0000

    criando a classse funcionarios

--- programa em c sharp/Program.cs
+++ programa em c sharp/Program.cs
@@ -115,6 +115,97 @@ class Zoologico
         Console.WriteLine("Registro animal, removido com sucesso!");
         Console.ResetColor();
     }
+    public void inserirFuncionarios(string nome, int numero, string sexo, int nasc, string departamento, string habilidades, double salario)
+    {
+        Funcionarios f = new Funcionarios(nome, numero, sexo, nasc, departamento, habilidades, salario);
+        if(f == null){
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Erro ao inserir, registro!!!");
+            Console.ResetColor();
+            return;
+        }
+        listaFuncionarios.Add(f);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Registro do funcionario, adicionado com sucesso.");
+        Console.ResetColor();
+    }
+    public void listarFuncionarios(){
+        foreach (Funcionarios f in listaFuncionarios)
+        {
+            f.monstrarFuncionarios();
+        }
+    }
+    public void monstrarFuncionarioNome(string nome){
+        foreach(Funcionarios f in listaFuncionarios)
+        {
+            if(nome == f.getNomeFuncionario())
+            {
+                f.monstrarFuncionarios();
+            }
+        }
+    }
+    public void monstrarFuncionarioSexo(string sexo){
+        foreach (Funcionarios f in listaFuncionarios)
+        {
+            if (sexo == f.getSexoFuncionario())
+            {
+                f.monstrarFuncionarios();
+            }
+        }
+    }
+    public Animais pesquisaFuncionarioNome(string nome)
+    {
+        foreach (Funcionarios f in listaFuncionarios)
+        {
+            if (nome == f.getNomeFuncionario())
+            {
+                return f;
+            }
+        }
+        return null;
+    }
+
+    //mudar todos para funcionario
+    public void RemoverAnimalNome(string nome)
+    {   
+        Animais a = this.pesquisaAnimaisNome(nome);
+        if(a == null)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Erro, não foi possível encontrar o animal.");
+            Console.ResetColor();
+            return;
+        }
+        listaAnimais.Remove(a);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Registro animal, removido com sucesso!");
+        Console.ResetColor();
+    }
+    public Animais pesquisaAnimaisEspecie(string especie)
+    {
+        foreach (Animais a in listaAnimais)
+        {
+            if (especie == a.getEspecie())
+            {
+                return a;
+            }
+        }
+        return null;
+    }
+    public void RemoverAnimalEspecie(string especie)
+    {
+        Animais a = this.pesquisaAnimaisEspecie(especie);
+        if (a == null)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Erro ao remover animal.");
+            Console.ResetColor();
+        }
+        listaAnimais.Remove(a);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Registro animal, removido com sucesso!");
+        Console.ResetColor();
+    }
 }
 class Animais
 {
@@ -146,5 +237,36 @@ class Animais
     public string getEspecie()
     {
         return this.especie;
+    }
+}
+class Funcionarios
+{
+    string nome, departamento, habilidades, sexo;
+    int numero, nasc;
+    double salario;
+    public Funcionarios(string nome, int numero, string sexo, int nasc, string departamento, string habilidades, double salario)
+    {
+        this.nome = nome;
+        this.numero = numero;
+        this.sexo = sexo;
+        int.nasc = nasc;
+        this.departamento = departamento;
+        this.habilidades = habilidades;
+        this.salario = salario;
+    }
+    public void monstrarFuncionarios()
+    {
+        Console.WriteLine("----------------------");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Lista dos Funcionarios");
+        Console.ResetColor();
+        Console.WriteLine("----------------------");
+        Console.WriteLine("Nome: {0}", this.nome);
+        Console.WriteLine("Número: {0}",this.numero);
+        Console.WriteLine("Sexo: {0}", this.sexo);
+        Console.WriteLine("Idade: {0}", this.nasc);
+        Console.WriteLine("Departamento: {0}", this.departamento);
+        Console.WriteLine("Habilidades: {0}",this.habilidades);
+        Console.WriteLine("Salario: {0}", this.salario);
     }
 }
